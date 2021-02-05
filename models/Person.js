5804a9ba0f9c434d049317745798d2beecba2159
Joao Duarte <joao.quiaios.duarte@gmail.com>
2021-02-05 20:32:54 +0000

    create model person

--- models/Person.js
+++ models/Person.js
@@ -3,9 +3,27 @@ const { Schema, model } = mongoose;
 
 const personSchema = new Schema(
   {
-    name: String,
-    occupation: String,
-    catchPhrase: String,
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "name is required."],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required."],
+    },
+
+    description: String,
+    donations: [{ type: Schema.Types.ObjectId, ref: "Donation" }],
   },
   {
     timestamps: true,
