0a81d29f66fa693c8602335f2ef476673a604061
JoaoDuarte39 <joao.quiaios.duarte@gmail.com>
2021-02-06 17:37:27 +0000

    added seeds for company

---
+++ seeds/company.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const Company = require("../models/Company");
+const companies = [
+  {
+    name: "company of Joao",
+    email: "a@a.com",
+    password: "1234567",
+    taxNumber: "123456",
+  },
+  {
+    name: "company of Joaquim",
+    email: "a@a.com",
+    password: "1234567",
+    taxNumber: "123456",
+  },
+  {
+    name: "company of Lourenco",
+    email: "a@a.com",
+    password: "1234567",
+    taxNumber: "123456",
+  },
+];
+
+mongoose
+  .connect("mongodb://localhost/food-waste-project", { useNewUrlParser: true })
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo", err);
+  });
+
+Company.create(companies)
+  .then((company) => {
+    console.log(`Created ${company} Model`);
+
+    // Once created, close the DB connection
+    mongoose.connection.close();
+  })
+  .catch((err) =>
+    console.log(`An error occurred while creating books from the DB: ${err}`)
+  );
